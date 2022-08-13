cba985acc72362ea45bdc1d5ba52900cf02f920d
Yonic Surny <ysu@aelptos.com>
2022-08-13 16:08:42 +0200

    Remove `LoadingViewController` as it is not used anymore

--- GPXExporter/UI/Routers/Router.swift
+++ GPXExporter/UI/Routers/Router.swift
@@ -11,8 +11,6 @@ protocol RouterProtocol {
     func showShare(for path: URL)
     func showInfo()
     func hideInfo()
-    func showProgress()
-    func hideProgress()
 }
 
 final class Router {
@@ -68,43 +66,5 @@ extension Router: RouterProtocol {
 
     func hideInfo() {
         navigationController.dismiss(animated: true)
-    }
-
-    func showProgress() {
-        synchronized(self) {
-            guard !self.isShowingProgres else { return }
-            self.isShowingProgres = true
-            DispatchQueue.main.async {
-                let loadingVC = LoadingViewController()
-                loadingVC.modalPresentationStyle = .overCurrentContext
-                loadingVC.modalTransitionStyle = .crossDissolve
-                self.navigationController.present(
-                    loadingVC,
-                    animated: true,
-                    completion: nil
-                )
-            }
-        }
-    }
-
-    func hideProgress() {
-        synchronized(self) {
-            guard self.isShowingProgres else { return }
-            DispatchQueue.main.async {
-                self.navigationController.dismiss(animated: true) {
-                    self.isShowingProgres = false
-                }
-            }
-        }
-    }
-}
-
-private extension Router {
-    @discardableResult
-    func synchronized<T>(_ lock: AnyObject, closure: () -> T) -> T {
-        objc_sync_enter(lock)
-        defer { objc_sync_exit(lock) }
-
-        return closure()
     }
 }
