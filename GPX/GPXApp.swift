a098999401d7038e9f9c7237116067f354328268
Yonic Surny <ysu@aelptos.com>
2023-03-24 20:49:54 +0100

    Enable app for macOS and iPadOS

--- GPX/GPXApp.swift
+++ GPX/GPXApp.swift
@@ -8,18 +8,24 @@ import SwiftUI
 struct GPXApp: App {
     var body: some Scene {
         WindowGroup {
-            TabView {
+            if UIDevice.current.userInterfaceIdiom == .phone {
+                TabView {
+                    NavigationStack {
+                        GPXViewer()
+                    }
+                    .tabItem {
+                        Label("viewer.title".localized, systemImage: "map")
+                    }
+                    NavigationStack {
+                        WorkoutsView(healthKitHelper: HealthKitHelper())
+                    }
+                    .tabItem {
+                        Label("workouts.title".localized, systemImage: "figure.run")
+                    }
+                }
+            } else {
                 NavigationStack {
                     GPXViewer()
-                }
-                .tabItem {
-                    Label("viewer.title".localized, systemImage: "map")
-                }
-                NavigationStack {
-                    WorkoutsView(healthKitHelper: HealthKitHelper())
-                }
-                .tabItem {
-                    Label("workouts.title".localized, systemImage: "figure.run")
                 }
             }
         }
